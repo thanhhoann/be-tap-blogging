b8d4b142ef62ee065fe7c1f8b2c71580b07e2385
thanhhoann <pdth456@gmail.com>
2024-03-03 21:19:40 +0700

    feat: add post by changing getSession to getServerSession

---
+++ pages/api/post/index.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth/next';
+import prisma from '../../../lib/prisma';
+import { authOptions } from '../auth/[...nextauth]';
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+    const { title, content } = req.body;
+
+    const session = await getServerSession(req, res, authOptions);
+    const result = await prisma.post.create({
+        data: {
+            title: title,
+            content: content,
+            author: { connect: { email: session?.user?.email } },
+        },
+    });
+    res.json(result);
+}

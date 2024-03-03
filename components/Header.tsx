c1d8dae08a03d8358edce813c46e369382cf8cae
thanhhoann <pdth456@gmail.com>
2024-03-03 12:00:49 +0700

    first commit 🚀

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,19 +2,21 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const Header: React.FC = () => {
-  const router = useRouter();
-  const isActive: (pathname: string) => boolean = (pathname) =>
-    router.pathname === pathname;
+type AppProps = {}
 
-  let left = (
-    <div className="left">
-      <Link href="/">
-        <a className="bold" data-active={isActive("/")}>
-          Feed
-        </a>
-      </Link>
-      <style jsx>{`
+const Header = () => {
+    const router = useRouter();
+    const isActive: (pathname: string) => boolean = (pathname) =>
+        router.pathname === pathname;
+
+    let left = (
+        <div className="left">
+            <Link href="/">
+                <a className="bold" data-active={isActive("/")}>
+                    Feed
+                </a>
+            </Link>
+            <style jsx>{`
         .bold {
           font-weight: bold;
         }
@@ -33,24 +35,24 @@ const Header: React.FC = () => {
           margin-left: 1rem;
         }
       `}</style>
-    </div>
-  );
+        </div>
+    );
 
-  let right = null;
+    let right = null;
 
-  return (
-    <nav>
-      {left}
-      {right}
-      <style jsx>{`
+    return (
+        <nav>
+            {left}
+            {right}
+            <style jsx>{`
         nav {
           display: flex;
           padding: 2rem;
           align-items: center;
         }
       `}</style>
-    </nav>
-  );
+        </nav>
+    );
 };
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,42 +4,42 @@ import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 
 export const getStaticProps: GetStaticProps = async () => {
-  const feed = [
-    {
-      id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-      published: false,
-      author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
-      },
-    },
-  ]
-  return { 
-    props: { feed }, 
-    revalidate: 10 
-  }
+    const feed = [
+        {
+            id: "1",
+            title: "Prisma is the perfect ORM for Next.js",
+            content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+            published: false,
+            author: {
+                name: "Nikolas Burk",
+                email: "burk@prisma.io",
+            },
+        },
+    ]
+    return {
+        props: { feed },
+        revalidate: 10
+    }
 }
 
-type Props = {
-  feed: PostProps[]
+type BlogProps = {
+    feed: PostProps[]
 }
 
-const Blog: React.FC<Props> = (props) => {
-  return (
-    <Layout>
-      <div className="page">
-        <h1>Public Feed</h1>
-        <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
+const Blog = (props: BlogProps) => {
+    return (
+        <Layout>
+            <div className="page">
+                <h1>Public Feed</h1>
+                <main>
+                    {props.feed.map((post) => (
+                        <div key={post.id} className="post">
+                            <Post post={post} />
+                        </div>
+                    ))}
+                </main>
             </div>
-          ))}
-        </main>
-      </div>
-      <style jsx>{`
+            <style jsx>{`
         .post {
           background: white;
           transition: box-shadow 0.1s ease-in;
@@ -53,8 +53,8 @@ const Blog: React.FC<Props> = (props) => {
           margin-top: 2rem;
         }
       `}</style>
-    </Layout>
-  )
+        </Layout>
+    )
 }
 
 export default Blog

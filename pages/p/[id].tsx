838553c61264377a7308e464d3a53036d246c235
thanhhoann <pdth456@gmail.com>
2024-03-03 21:27:05 +0700

    feat: add delete functionality

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -8,51 +8,64 @@ import { useSession } from 'next-auth/react';
 import prisma from '../../lib/prisma';
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const post = await prisma.post.findUnique({
-    where: {
-      id: String(params?.id),
-    },
-    include: {
-      author: {
-        select: { name: true, email: true },
-      },
-    },
-  });
-  return {
-    props: post,
-  };
+    const post = await prisma.post.findUnique({
+        where: {
+            id: String(params?.id),
+        },
+        include: {
+            author: {
+                select: { name: true, email: true },
+            },
+        },
+    });
+    return {
+        props: post,
+    };
 };
 
 async function publishPost(id: string): Promise<void> {
-  await fetch(`/api/publish/${id}`, {
-    method: 'PUT',
-  });
-  await Router.push('/');
+    await fetch(`/api/publish/${id}`, {
+        method: 'PUT',
+    });
+    await Router.push('/');
+}
+
+async function deletePost(id: string): Promise<void> {
+    await fetch(`/api/post/${id}`, {
+        method: 'DELETE',
+    });
+    Router.push('/');
 }
 
 const Post: React.FC<PostProps> = (props) => {
-  const { data: session, status } = useSession();
-  if (status === 'loading') {
-    return <div>Authenticating ...</div>;
-  }
-  const userHasValidSession = Boolean(session);
-  const postBelongsToUser = session?.user?.email === props.author?.email;
-  let title = props.title;
-  if (!props.published) {
-    title = `${title} (Draft)`;
-  }
+    const { data: session, status } = useSession();
+    if (status === 'loading') {
+        return <div>Authenticating ...</div>;
+    }
+    const userHasValidSession = Boolean(session);
+    const postBelongsToUser = session?.user?.email === props.author?.email;
+    let title = props.title;
+    if (!props.published) {
+        title = `${title} (Draft)`;
+    }
 
-  return (
-    <Layout>
-      <div>
-        <h2>{title}</h2>
-        <p>By {props?.author?.name || 'Unknown author'}</p>
-        <ReactMarkdown children={props.content} />
-        {!props.published && userHasValidSession && postBelongsToUser && (
-          <button onClick={() => publishPost(props.id)}>Publish</button>
-        )}
-      </div>
-      <style jsx>{`
+    return (
+        <Layout>
+            <div>
+                <h2>{title}</h2>
+                <p>By {props?.author?.name || 'Unknown author'}</p>
+                <ReactMarkdown children={props.content} />
+
+                {!props.published && userHasValidSession && postBelongsToUser && (
+                    <button onClick={() => publishPost(props.id)}>Publish</button>
+                )}
+
+                {userHasValidSession && postBelongsToUser && (
+                    <button onClick={() => deletePost(props.id)}>Delete</button>
+                )}
+
+            </div>
+            <style jsx>{`
         .page {
           background: var(--geist-background);
           padding: 2rem;
@@ -73,8 +86,8 @@ const Post: React.FC<PostProps> = (props) => {
           margin-left: 1rem;
         }
       `}</style>
-    </Layout>
-  );
+        </Layout>
+    );
 };
 
 export default Post;
